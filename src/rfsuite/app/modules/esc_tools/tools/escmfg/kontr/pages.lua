f4892f2d8f456bff0ca53d1fcdecb7a1381c5255
garfield2412 <kay.koehler@gmx.de>
2026-02-25 00:30:49 +0100

    remove unused fields2

--- src/rfsuite/app/modules/esc_tools/tools/escmfg/kontr/pages.lua
+++ src/rfsuite/app/modules/esc_tools/tools/escmfg/kontr/pages.lua
@@ -7,7 +7,7 @@ local PageFiles = {}
 
 PageFiles[#PageFiles + 1] = {title = "@i18n(app.modules.esc_tools.mfg.kontr.basic)@", script = "esc_basic.lua", image = "basic.png"}
 PageFiles[#PageFiles + 1] = {title = "@i18n(app.modules.esc_tools.mfg.kontr.motor)@", script = "esc_motor.lua", image = "motor.png"}
-PageFiles[#PageFiles + 1] = {title = "@i18n(app.modules.esc_tools.mfg.kontr.alarm)@", script = "esc_alarm.lua", image = "limits.png"}
+--PageFiles[#PageFiles + 1] = {title = "@i18n(app.modules.esc_tools.mfg.kontr.alarm)@", script = "esc_alarm.lua", image = "limits.png"}
 PageFiles[#PageFiles + 1] = {title = "@i18n(app.modules.esc_tools.mfg.kontr.rc)@", script = "esc_rc.lua", image = "basic.png"}
 PageFiles[#PageFiles + 1] = {title = "@i18n(app.modules.esc_tools.mfg.kontr.esc_status)@", script = "esc_status.lua", image = "other.png"}
 
